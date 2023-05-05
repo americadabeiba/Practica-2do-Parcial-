c226be583bb4f7d065ef5c62e155d3e9cffcb0d5
americadabeiba <Dabeiba.Olazo@fundacion-jala.org>
2023-05-05 08:54:57 -0400

    Ejercicio2

---
+++ src/ArbolPeorCaso.java
@@ -0,0 +1,24 @@
+import Lib.Arbol;
+
+public class ArbolPeorCaso {
+    public static void main(String[] args) {
+        String[][] ordenamientos = {
+                {"A", "X", "C", "S", "E", "R", "H"},
+                {"A", "C", "E", "H", "R", "S", "X"},
+                {"X", "S", "R", "H", "E", "C", "A"},
+                {"A", "X", "C", "E", "H", "S", "R"},
+                {"R", "S", "X", "H", "C", "A", "E"},
+                {"A", "H", "E", "S", "R", "C", "X"}
+        };
+
+        for (int i = 0; i < ordenamientos.length; i++) {
+            Arbol<String, Integer> arbol = new Arbol<>();
+            for (String llave : ordenamientos[i]) {
+                arbol.insertar(llave, 1);
+            }
+            System.out.println("Ordenamiento " + (i + 1) + ":");
+            arbol.mostrarPiramide();
+            System.out.println();
+        }
+    }
+}
